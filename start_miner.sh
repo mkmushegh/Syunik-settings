5a37da2f87ff157d7f8086509928679d0c026626
Mushegh <mkrtchyan.mushegh@gmail.com>
2017-10-01 18:56:02 +0400

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-/home/syunik1/Desktop/0.3.4b/miner --server eu1-zcash.flypool.org --user t1RJ3hvzPQsVHe89yjiTo9XbyfFMbCz8EKa.syunik1_ubuntu --pass x --port 3333
-
+/home/syunik2/Desktop/0.3.4b/miner --server eu1-zcash.flypool.org --user t1RJ3hvzPQsVHe89yjiTo9XbyfFMbCz8EKa.syunik2_ubuntu --pass x --port 3333 --intensity 64 --tempunits C --templimit 75
