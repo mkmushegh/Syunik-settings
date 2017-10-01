2802d76013f9c029337fc0654efaf9e6b994bad2
Mushegh <mkrtchyan.mushegh@gmail.com>
2017-10-01 18:42:16 +0400

    Update oc1070.sh

--- oc1070.sh
+++ oc1070.sh
@@ -7,7 +7,7 @@ nvidia-settings -a '[gpu:0]/GPUFanControlState=1'
 nvidia-settings -a '[fan:0]/GPUTargetFanSpeed=90'
 nvidia-smi -i 0 -pl 166 #max 170, don't go higher please
 
-nvidia-settings -a '[gpu:1]/GPUGraphicsClockOffset[3]=180'
+nvidia-settings -a '[gpu:1]/GPUGraphicsClockOffset[3]=175'
 nvidia-settings -a '[gpu:1]/GPUMemoryTransferRateOffset[3]=1400' #max 1400, don't go higher please
 nvidia-settings -a '[gpu:1]/GPUPowerMizerMode=1'
 nvidia-settings -a '[gpu:1]/GPUFanControlState=1'
@@ -15,28 +15,28 @@ nvidia-settings -a '[fan:1]/GPUTargetFanSpeed=90'
 nvidia-smi -i 1 -pl 166 #max 170, don't go higher please
 
 #Strix gtx 1070 OC edition
-nvidia-settings -a '[gpu:2]/GPUGraphicsClockOffset[3]=150' #OC edition clock offset is less than ordinary Gameing edition
+nvidia-settings -a '[gpu:2]/GPUGraphicsClockOffset[3]=140' #OC edition clock offset is less than ordinary Gameing edition
 nvidia-settings -a '[gpu:2]/GPUMemoryTransferRateOffset[3]=1400' #max 1400, don't go higher please
 nvidia-settings -a '[gpu:2]/GPUPowerMizerMode=1'
 nvidia-settings -a '[gpu:2]/GPUFanControlState=1'
 nvidia-settings -a '[fan:2]/GPUTargetFanSpeed=90'
 nvidia-smi -i 2 -pl 190 #max 200, don't go higher please
 
-nvidia-settings -a '[gpu:3]/GPUGraphicsClockOffset[3]=180'
+nvidia-settings -a '[gpu:3]/GPUGraphicsClockOffset[3]=175'
 nvidia-settings -a '[gpu:3]/GPUMemoryTransferRateOffset[3]=1400' #max 1400, don't go higher please
 nvidia-settings -a '[gpu:3]/GPUPowerMizerMode=1'
 nvidia-settings -a '[gpu:3]/GPUFanControlState=1'
 nvidia-settings -a '[fan:3]/GPUTargetFanSpeed=90'
 nvidia-smi -i 3 -pl 166 #max 170, don't go higher please
 
-nvidia-settings -a '[gpu:4]/GPUGraphicsClockOffset[3]=180'
+nvidia-settings -a '[gpu:4]/GPUGraphicsClockOffset[3]=175'
 nvidia-settings -a '[gpu:4]/GPUMemoryTransferRateOffset[3]=1400' #max 1400, don't go higher please
 nvidia-settings -a '[gpu:4]/GPUPowerMizerMode=1'
 nvidia-settings -a '[gpu:4]/GPUFanControlState=1'
 nvidia-settings -a '[fan:4]/GPUTargetFanSpeed=90'
 nvidia-smi -i 4 -pl 166 #max 170, don't go higher please
 
-nvidia-settings -a '[gpu:5]/GPUGraphicsClockOffset[3]=180'
+nvidia-settings -a '[gpu:5]/GPUGraphicsClockOffset[3]=175'
 nvidia-settings -a '[gpu:5]/GPUMemoryTransferRateOffset[3]=1400' #max 1400, don't go higher please
 nvidia-settings -a '[gpu:5]/GPUPowerMizerMode=1'
 nvidia-settings -a '[gpu:5]/GPUFanControlState=1'
