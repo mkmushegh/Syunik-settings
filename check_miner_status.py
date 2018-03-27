34f3fea63aeb9ef5fde4e6489d3d1b14165ea5ee
Mushegh <mkrtchyan.mushegh@gmail.com>
2018-03-27 21:32:12 +0400

    improvement of the script
    lots of improvements

--- check_miner_status.py
+++ check_miner_status.py
@@ -10,6 +10,14 @@ import requests
 from time import sleep 
 import smtplib
 import os
+import datetime
+
+def restartMiner():
+   os.system("taskkill /f /im  miner.exe")
+   sleep(5)
+   os.chdir("C:/Users/Syunik1/Desktop/0.3.4b")
+   os.startfile("fypool.bat")
+   sleep(60)
 
 def send_email(reason):
    try:
@@ -23,7 +31,7 @@ def send_email(reason):
       #Send the mail
       sent_from = 'soltiteam@gmail.com'
       to = ['mkrtchyan.mushegh@gmail.com', 'tigranmur@gmail.com']
-      message = 'Subject: {}\n\n{}'.format('Restart!', 'snk3 was restarted, reason: {}'.format(reason))
+      message = 'Subject: {}\n\n{}'.format('Restart!', 'snk1 was restarted, reason: {}'.format(reason))
       server.sendmail(sent_from, to, message)
       server.quit()
    except:
@@ -33,8 +41,12 @@ def send_email(reason):
 print('will start miner check within 60 sec...')
 sleep(60)
 errorCounter = 0
+os.environ['NO_PROXY'] = '127.0.0.1'
 while True:
   hashrates = []
+  temps = []
+  powers = []
+  shares = []
   try:
     r = requests.get('http://127.0.0.1:42000/getstat')
     json = r.json()
@@ -44,15 +56,38 @@ while True:
     print('number of GPUs: {}'.format(numberOfGPUs))
     for i in range(numberOfGPUs):
       currentGPUhashrate = results[i]['speed_sps']
+      currentGPUtemp = results[i]['temperature']
+      currentGPUpower = results[i]['gpu_power_usage']
+      currentAcceptedShares = results[i]['accepted_shares']
       hashrates.append(currentGPUhashrate)
-      print('GPU{}: {}'.format(i, currentGPUhashrate))
+      temps.append(currentGPUtemp)
+      powers.append(currentGPUpower)
+      shares.append(currentAcceptedShares)
+      print('GPU{}: {}, accepted shares: {}'.format(i, currentGPUhashrate, currentAcceptedShares))
     #print('error: {}'.format(error))
 
+    print('Total hashrate: {}, total shares: {}'.format(sum(hashrates), sum(shares)))
+
+    try:
+       r2 = requests.post("http://artkaren.com/", 
+            data={'miner': 'Syunik1',
+                 'gpu0': hashrates[0], 'gpu0temp': temps[0], 'gpu0power': powers[0],
+                 'gpu1': hashrates[1], 'gpu1temp': temps[1], 'gpu1power': powers[1],
+                 'gpu2': hashrates[2], 'gpu2temp': temps[2], 'gpu2power': powers[2],
+                 'gpu3': hashrates[3], 'gpu3temp': temps[3], 'gpu3power': powers[3],
+                 'gpu4': hashrates[4], 'gpu4temp': temps[4], 'gpu4power': powers[4],
+                 'gpu5': hashrates[5], 'gpu5temp': temps[5], 'gpu5power': powers[5],
+                 'pytime': datetime.datetime.utcnow()})
+       print(r2.status_code, r2.reason)
+       #print(r2.text)
+    except:
+       print('Error sending data to the server')
+    
     hashrates.sort()
-    if hashrates[0] < 350:
-      print("one of the GPU's hashrate is less than 350 Sol/s, restart...")
-      send_email("one of the GPU's hashrate is less than 350 Sol/s")
-      os.system('reboot') 
+    if hashrates[0] < 100:
+      print("one of the GPU's hashrate is less than 100 Sol/s, restart...")
+      send_email("one of the GPU's hashrate is less than 100 Sol/s")
+      restartMiner()
     sleep(30)
   except:
     if errorCounter < 5:
@@ -63,5 +98,6 @@ while True:
       print('too many errors, restarting the system in 10 sec...')
       send_email("error connecting to http://127.0.0.1:42000 to many times")
       sleep(10)
-      os.system('reboot') 
+      errorCounter = 0
+      restartMiner()
 
